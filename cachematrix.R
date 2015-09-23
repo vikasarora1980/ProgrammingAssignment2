0c9ec38a09d812f989b5e97a596a6b649e66ede0
Vikas Arora <vikasarora1980@gmail.com>
2015-09-24 00:53:07 +0530

    Programming Assignment 2 by Vikas Arora

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix inversion is usually a costly computation and there can be some benefit to caching the inverse 
+# of a matrix rather than compute it repeatedly. 
+# This assignment is to write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+# The first function, makeCacheMatrix creates a special "matrix" object that can cache its inverse. 
+# It's actually a list containing a function to
+# set the value of the vector
+# get the value of the vector
+# set the value of the mean
+# get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_x <- NULL #Initialize inv_x as null 
+        set <- function(y) { #Set Value for Matrix
+                x <<- y
+                inv_x <<- NULL
+        }
+        get <- function() x #Get Value for matrix
+        setinv <- function(inverse) inv_x <<- inverse #Set Inverse of matrix
+        getinv <- function() inv_x #Get inverse of matrix
+        list(set = set, get = get, #List values and their inverse
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), 
+# then the cachesolve would retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv_x <- x$getinv()  #Invoke getinv function
+        if(!is.null(inv_x)) { #Check whether existing value of inverse available
+                message("getting cached data") #Inverse available in Cache
+                return(inv_x) #Return inverse from Cache
+        }
+        data <- x$get()  #Invoke value of matrix in data variable
+        inv_x <- solve(data, ...) # Calculate Inverse of matrix from data variable
+        x$setinv(inv_x) #Set value of inverse of matrix 
+        inv_x #Return Inverse
 }
